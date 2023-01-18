e789bbe5ba454acf2776ed0a3ef042cb292a1048
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-01-18 02:17:31 -0500

    up

--- rhudbg/v2/rhuMacros.svh
+++ rhudbg/v2/rhuMacros.svh
@@ -6,5 +6,15 @@
 		debug.log(this.get_inst_id(),msg,`__FILE__,`__LINE__); \
 	`endif
 
+`define debugCall(extMsg,line) \
+	`ifndef RHUDBG_DISABLE \
+		begin \
+			string msg = {`"line`",", ",extMsg}; \
+			`debug($sformatf("calling %s",msg)) \
+			line; \
+		end
+	`else
+		line;
+	`endif
 
 `endif

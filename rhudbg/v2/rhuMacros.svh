a8847d4eff15910abbbc898333fb8fc87146c391
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-01-18 00:35:39 -0500

    add macro guard, can be disabled by RHUDBG_DISABLE

--- rhudbg/v2/rhuMacros.svh
+++ rhudbg/v2/rhuMacros.svh
@@ -2,6 +2,9 @@
 `define rhuMacros__svh
 
 `define debug(msg) \
-	debug.log(this.get_inst_id(),msg,`__FILE__,`__LINE__);
+	`ifndef RHUDEBUG_DISABLE \
+		debug.log(this.get_inst_id(),msg,`__FILE__,`__LINE__); \
+	`endif
+
 
 `endif
